ff2b35c7584737631af82934cdf55b87d46e3f46
pedrohassen <pedro_hasse@yahoo.com.br>
2024-01-24 23:51:08 -0300

    feat: 🦾 completes the routes with auth middleware

--- be-api/start/routes.js
+++ be-api/start/routes.js
@@ -7,19 +7,19 @@ Route.get('/', () => {
 })
 
 Route.post('user', 'AuthController.register')
-Route.post('auth', 'AuthController.authenticate')
+Route.post('login', 'AuthController.authenticate')
 
-Route.post('book', 'BooksController.store')
+Route.post('book', 'BooksController.store').middleware(['auth']);
 Route.get('books', 'BooksController.index')
 Route.get('book/:id', 'BooksController.show')
-Route.put('book/:id', 'BooksController.update')
-Route.delete('book/:id', 'BooksController.destroy')
+Route.put('book/:id', 'BooksController.update').middleware(['auth']);
+Route.delete('book/:id', 'BooksController.destroy').middleware(['auth']);
 
-Route.post('client', 'ClientsController.store')
-Route.get('clients', 'ClientsController.index')
-Route.get('client/:id', 'ClientsController.show')
-Route.put('client/:id', 'ClientsController.update')
-Route.get('client/detail/:id', 'ClientsController.detail')
-Route.delete('client/:id', 'ClientsController.destroy')
+Route.post('client', 'ClientsController.store').middleware(['auth']);
+Route.get('clients', 'ClientsController.index').middleware(['auth']);
+Route.get('client/:id', 'ClientsController.show').middleware(['auth']);
+Route.put('client/:id', 'ClientsController.update').middleware(['auth']);
+Route.get('client/detail/:id', 'ClientsController.detail').middleware(['auth']);
+Route.delete('client/:id', 'ClientsController.destroy').middleware(['auth']);
 
-Route.post('sale', 'SalesController.store')
+Route.post('sale', 'SalesController.store').middleware(['auth']);
